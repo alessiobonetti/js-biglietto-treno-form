9a697ce3dba8faf8afab62d3d36dadc38702f6f4
Alessio Bonetti <alesine87@gmail.com>
2020-08-07 18:00:58 +0200

    inizio Js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// buttons
+
+var = btnGenera = document.getElementById("btn_genera");
+btnGenera.addEventListener("click"
+ function() {
+   var getName = document.getElementById("client_name").value;
+   var getKm = document.getElementById("client_km").value;
+   var getAge = document.getElementById("client_age").value;
+ }
+);
