a314c97baa33ad5e2a750be52244e0d49f4851e3
Alessio Bonetti <alesine87@gmail.com>
2020-08-07 18:18:06 +0200

    js button genera prende le info e fa apparire section

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 // buttons
 
-var = btnGenera = document.getElementById("btn_genera");
-btnGenera.addEventListener("click"
+var btnGenera = document.getElementById("btn_genera");
+btnGenera.addEventListener("click",
  function() {
    var getName = document.getElementById("client_name").value;
    var getKm = document.getElementById("client_km").value;
    var getAge = document.getElementById("client_age").value;
+
+   document.getElementById("ticket_calc").classList.remove("dsp_none");
+
+ }
+);
+
+var btnGenera = document.getElementById("btn_annulla");
+btnGenera.addEventListener("click",
+ function() {
+   
+
+   document.getElementById("ticket_calc").classList.add("dsp_none");
+
  }
 );
